d659916129a3cd1df72d0575a7b5e2319cb580a5
Aryan Hulawale <152802583+AryanHulawale@users.noreply.github.com>
2025-09-27 12:01:18 +0530

    Update gemini.js

--- server/config/gemini.js
+++ server/config/gemini.js
@@ -1,21 +1,28 @@
-import { GoogleGenAI } from '@google/genai';
+import { GoogleGenerativeAI } from "@google/generative-ai";
 import dotenv from "dotenv";
-dotenv.config(); 
+dotenv.config();
 
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
-const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
+if (!GEMINI_API_KEY) {
+  throw new Error(" GEMINI_API_KEY is missing in .env file");
+}
+
+const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 
 async function main(prompt) {
-  const response = await ai.models.generateContent({
-    model: 'gemini-2.0-flash-001',
-    contents: prompt,
-  });
-  // console.log("repsonse is comming")
-  // console.log(response.text);
-  return response.text; 
+  try {
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-001" });
+
+    const result = await model.generateContent([prompt]);
+
+    const text = result.response.text(); 
+
+    return text;
+  } catch (error) {
+    console.error("Gemini API Error:", error);
+    return null;
+  }
 }
 
 export default main;
-
-
